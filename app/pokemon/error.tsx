983d37bee1b2c04dffd7894b00a2cc69c98bd78a
Eli Tamosauskas <eligijus@cast.ai>
2026-03-23 11:57:42 +0200

    add error recovery

--- app/pokemon/error.tsx
+++ app/pokemon/error.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function PokemonError() {
+  const router = useRouter();
+
   return (
     <div>
       <h3 className="mb-5 text-xl text-red-500">
         ❓❓❓ Something went wrong here.
       </h3>
       <p>Try again later...</p>
+
+      <button onClick={() => router.back()} className="underline p-5">
+        Go Back
+      </button>
     </div>
   );
 }
