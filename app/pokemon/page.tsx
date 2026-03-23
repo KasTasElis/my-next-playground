bc1d6e747e8bf9a9b6e6c7b321e1ff075b6da7e2
Eli Tamosauskas <eligijus@cast.ai>
2026-03-23 10:11:57 +0200

    routing

---
+++ app/pokemon/page.tsx
@@ -0,0 +1,15 @@
+export default function Pokemon() {
+  return (
+    <div>
+      <h3>This is the Pokemon index:</h3>
+
+      {/* <ul>
+        {posts.map((post) => (
+          <li key={post}>
+            <Link href={`/blog/${post}`}>{post}</Link>
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
+}
