2f8e9dc802e5317d69f39eb2831d350edce21b45
Eli Tamosauskas <eligijus@cast.ai>
2026-03-23 11:27:07 +0200

    add pokemon error page

---
+++ app/pokemon/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function PokemonError() {
+  return (
+    <div>
+      <h3 className="mb-5 text-xl text-red-500">
+        ❓❓❓ Something went wrong here.
+      </h3>
+      <p>Try again later...</p>
+    </div>
+  );
+}
